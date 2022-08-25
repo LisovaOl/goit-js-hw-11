f6a0c946bb54c71eb4a064466380e850fd067e20
LisovaOl <lisova.olenka@gmail.com>
2022-08-25 12:15:02 +0300

    start copy

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+{/* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>; */}
